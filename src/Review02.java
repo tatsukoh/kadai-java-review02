5fcd60d59e18dad9abb17b526001ff2491287ac5
tatsu <tatsu@192.168.1.34>
2023-02-02 20:52:17 +0900

    再提出

--- src/Review02.java
+++ src/Review02.java
@@ -3,9 +3,11 @@ public class Review02 {
 
     public static void main(String[] args) {
         for(int i = 1; i <= 100; i++) {
-            if(i % 3 == 0) {
-                System.out.println("FIzz");
+            if(i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FIzzBuzz");
             }else if(i % 5 == 0) {
+                System.out.println("Buzz");
+            }else if(i % 5 ==0) {
                 System.out.println("Buzz");
             }else {
                 System.out.println(i);
